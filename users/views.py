2156c5cf1bd5cc54aff8a12aa8cad8f6dbd0d44a
Om Dahale <om.g.dahale@gmail.com>
2021-10-12 14:57:03 +0530

    fix profile picture not updating

--- users/views.py
+++ users/views.py
@@ -3,16 +3,17 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import UserUpdateForm
 
+
 @login_required
 def profile(request):
-    if request.method == 'POST':
-        form = UserUpdateForm(request.POST, instance=request.user)
+    if request.method == "POST":
+        form = UserUpdateForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated successfully.')
+            messages.success(request, "Profile updated successfully.")
 
-            return redirect('account_profile')
+            return redirect("account_profile")
     else:
         form = UserUpdateForm(instance=request.user)
 
-    return render(request, 'account/profile.html', {'form': form})
+    return render(request, "account/profile.html", {"form": form})
