73361c0090e1f38e9d003689c93f07ecb7592bb8
Om Dahale <om.g.dahale@gmail.com>
2021-10-14 23:20:19 +0530

    send and receive messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,6 +1,7 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.utils import timezone
 from .models import Chat
 
 
@@ -10,26 +11,56 @@ class ChatConsumer(WebsocketConsumer):
             self.close()
         self.user = self.scope["user"]
         async_to_sync(self.channel_layer.group_add)("global", self.channel_name)
+
+        for chat in self.user.chats.all():
+            async_to_sync(self.channel_layer.group_add)(
+                f"chat_{chat.pk}", self.channel_name
+            )
         self.accept()
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)("global", self.channel_name)
+        for chat in self.user.chats.all():
+            async_to_sync(self.channel_layer.group_discard)(
+                f"chat_{chat.pk}", self.channel_name
+            )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        chat = Chat.objects.get(pk=text_data_json["chat_pk"])
+        if chat.members.filter(pk=self.user.pk).exists():
+            message = text_data_json["message"]
+            now = timezone.now()
 
-        self.send(text_data=json.dumps({"message": message}))
+            async_to_sync(self.channel_layer.group_send)(
+                f"chat_{chat.pk}",
+                {
+                    "type": "new_message",
+                    "chat_pk": chat.pk,
+                    "author": self.user.username,
+                    "author_img_url": self.user.profile_pic.url,
+                    "datetime": now.isoformat(),
+                    "message": message,
+                },
+            )
 
     def new_chat(self, event):
         chat = Chat.objects.get(pk=event["chat_pk"])
         if chat.members.filter(pk=self.user.pk).exists():
+            async_to_sync(self.channel_layer.group_add)(
+                f"chat_{chat.pk}", self.channel_name
+            )
             self.send(
                 text_data=json.dumps(
                     {
                         "command": "add_chat",
+                        "chat_pk": chat.pk,
                         "name": chat.get_name(self.user),
                         "image_url": chat.get_image(self.user).url,
                     }
                 )
             )
+
+    def new_message(self, event):
+        event["command"] = "new_message"
+        self.send(text_data=json.dumps(event))
--- chat/static/chat/js/chat.js
+++ chat/static/chat/js/chat.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    function addChat(name, image_url){
-        const $chat = $(`<li class="contact">
+    function addChat(chat_pk, name, image_url){
+        const $chat = $(`<li id="chat_${chat_pk}" class="contact" data-pk=${chat_pk}>
             <div class="wrap">
               <img src="${image_url}" alt="" />
               <div class="meta">
@@ -13,6 +13,27 @@ $(document).ready(function(){
         $('#contacts ul').prepend($chat);
     }
 
+    function newMessage(chat_pk, author, author_img_url, datetime, message){
+        const name = author === username ? 'You' : author;
+        $(`#chat_${chat_pk}`).find('.preview').text(`${name}: ${message}`);
+
+        if (chat_pk === activeChatPk){
+            const dt = new Date(datetime).toLocaleString();
+            const source = author === username ? 'replies' : 'sent';
+            const $msg = $(`<li class="${source}">
+              <img src="${author_img_url}" alt="" />
+              <p>
+                <small class="small d-block mt-2"><span class="name"></span> | ${dt}</small>
+              </p>
+            </li>`);
+            $msg.find('p').prepend(document.createTextNode(message));
+            $msg.find('.name').text(name);
+            $('#chat-log').append($msg);
+        }
+    }
+
+    const username = JSON.parse(document.getElementById('username').textContent);
+    let activeChatPk = null;
     const chatSocket = new WebSocket(
         'ws://'
         + window.location.host
@@ -21,25 +42,27 @@ $(document).ready(function(){
 
     chatSocket.onmessage = function(e) {
         const data = JSON.parse(e.data);
-        // const $msg = $(`<li class="replies">
-        //   <img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
-        //   <p>
-        //     <small class="small d-block mt-2">Harvey Specter | Oct 2, 2021, 8:21 PM</small>
-        //   </p>
-        // </li>`);
-        // $msg.find('p').prepend(document.createTextNode(data.message));
-        // $('#chat-log').append($msg);
+
         if (data['command'] === 'add_chat') {
-            addChat(data['name'], data['image_url']);
+            addChat(data['chat_pk'], data['name'], data['image_url']);
+        }
+        else if (data['command'] === 'new_message') {
+            newMessage(
+                data['chat_pk'],
+                data['author'],
+                data['author_img_url'],
+                data['datetime'],
+                data['message']
+            );
         }
     };
 
     chatSocket.onclose = function(e) {
         console.error('Chat socket closed unexpectedly');
     };
+
     const $input = $('#chat-message-input');
     const $submit = $('#chat-message-submit');
-    $input.focus();
     $input.keyup(function(e) {
         if (e.keyCode === 13) {  // enter, return
             $submit.click();
@@ -47,10 +70,24 @@ $(document).ready(function(){
     });
 
     $submit.click(function(e) {
-        const message = $input.val();
-        chatSocket.send(JSON.stringify({
-            'message': message
-        }));
-        $input.val('');
+        const message = $input.val().trim();
+        if (message){
+            chatSocket.send(JSON.stringify({
+                'chat_pk': activeChatPk,
+                'message': message
+            }));
+            $input.val('');
+        }
+    });
+
+    $('.contact').click(function(e){
+        if (activeChatPk != $(this).data('pk')){
+            activeChatPk = $(this).data('pk');
+            $('.content').show();
+            $('.contact').removeClass('active');
+            $(this).addClass('active');
+            $('#chat-log').empty();
+            $input.focus();
+        }
     });
 });
