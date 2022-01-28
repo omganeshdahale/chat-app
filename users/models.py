9547da296bd16fe2f67adf1f34d29af9d3ee3dab
Om Dahale <om.g.dahale@gmail.com>
2022-01-28 12:42:23 +0530

    resize avatar

--- users/models.py
+++ users/models.py
@@ -1,7 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from PIL import Image
 
 
 class User(AbstractUser):
     profile_pic = models.ImageField(upload_to="profile_pics")
     about_me = models.CharField(max_length=100, default="Available")
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.profile_pic:
+            img = Image.open(self.profile_pic.path)
+            output_size = (96, 96)
+            img = img.resize(output_size)
+            img.save(self.profile_pic.path)
